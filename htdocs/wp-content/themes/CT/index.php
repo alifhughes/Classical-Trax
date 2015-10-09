3de948ed4fa7a19907a81b22637ae4891316765b
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2015-10-09 19:55:58 +0200

    Next and previous posts fix

--- htdocs/wp-content/themes/CT/index.php
+++ htdocs/wp-content/themes/CT/index.php
@@ -64,9 +64,6 @@
                         <li class="next">
                             <?php next_posts_link(); ?>
                         </li>
-                    </ul> 
-                    <!-- Pager -->
-                    <ul class="pager">
                         <li class="previous">
                             <?php previous_posts_link(); ?>
                         </li>
